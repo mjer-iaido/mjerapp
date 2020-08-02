f5ca567cec11b25ca705c2f387723a3907f66a5d
EC2 Default User <ec2-user@ip-10-0-0-154.us-east-2.compute.internal>
2020-08-02 06:44:09 +0000

    configured postgresql and etc

--- scoringsheets/scoringsheets/settings/common.py
+++ scoringsheets/scoringsheets/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -110,7 +110,8 @@ STATIC_URL = '/static/'
 # Fetch the project_root
 PROJECT_ROOT = os.path.dirname(BASE_DIR)
 # /path/to/project_directory/static_file
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, "static"))
 
 STATICFILES_DIRS = (
     os.path.normpath(os.path.join(BASE_DIR, "assets")),
--- scoringsheets/scoringsheets/settings/prod.py
+++ scoringsheets/scoringsheets/settings/prod.py
@@ -2,11 +2,17 @@ from .common import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'*',
+	]
 
 DATABASES = {
     'default': {
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mjerapp',
+        'USER': 'mjeriaido',
+        'PASSWORD': '1muso2jikiden',
+   }
 }
 
 INSTALLED_APPS += (
